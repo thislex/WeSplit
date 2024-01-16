e2f7c80b92ae2d27b372bd5b27689e5c33f05106
Lexter Tapawan <lexter.tapawan@gmail.com>
2024-01-16 10:04:43 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Lexter Tapawan on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
